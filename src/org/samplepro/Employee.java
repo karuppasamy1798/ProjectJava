0b0687cb855e1b836f32a1ea0d74ebd49594033c
karuppasamy1798 <karuppasamy1798@.com>
2021-10-15 08:41:49 +0530

    Some project added

---
+++ src/org/samplepro/Employee.java
@@ -0,0 +1,15 @@
+package org.samplepro;
+
+public class Employee {
+	
+     private void employeeName() {
+		System.out.println("EmployeeName:Karuppasamy");
+	}
+     
+     public static void main(String[] args) {
+    	 
+    	 Employee e = new Employee();
+		
+	}
+
+}
