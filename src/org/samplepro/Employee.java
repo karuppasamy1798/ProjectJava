392952cae8150188b3574426a96917e251c83797
karuppasamy1798 <karuppasamy1798@.com>
2021-10-16 08:06:01 +0530

    Project updated

--- src/org/samplepro/Employee.java
+++ src/org/samplepro/Employee.java
@@ -1,16 +1,20 @@
 package org.samplepro;
 
 public class Employee {
-	
-     private void employeeName() {
+
+	private void employeeName() {
 		System.out.println("EmployeeName:Karuppasamy");
 	}
-     
-     public static void main(String[] args) {
-    	 
-    	 Employee e = new Employee();
-    	 e.employeeName();
-		
+
+	private void employeeId() {
+		System.out.println("EmployeeID:ME1234");
+	}
+
+	public static void main(String[] args) {
+
+		Employee e = new Employee();
+		e.employeeName();
+
 	}
 
 }
